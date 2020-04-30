69e3c56e57763fd19a91a046083e34b1fe9a5d94
Justin Gargano <jlgargan@ncsu.edu>
2020-04-29 22:23:47 -0400

    Created helper functions file

--- screens/JoinGameMenuScreen.js
+++ screens/JoinGameMenuScreen.js
@@ -8,7 +8,7 @@ import firebase from 'firebase'
 import { TextButton, HeaderText } from "../components/StyledText";
 import { TitledPage } from "../components/Template";
 import { ScrollView } from "react-native-gesture-handler";
-import Loader from '../components/Loader'
+import Loader from '../components/Loader';
 
 export default function JoinGameMenuScreen({ navigation }) {
   const db = firebase.firestore();
